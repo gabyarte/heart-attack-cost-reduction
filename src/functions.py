7103e32b07e2988b68f7fef2279223b20e02379f
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2022-12-28 12:04:44 +0100

    feat: Add hierachical clustering analysis

--- src/functions.py
+++ src/functions.py
@@ -1,16 +1,50 @@
 import pandas as pd
 import numpy as np
 
-
-def inspect_nulls(df):
-    nulls = df.isna().sum()
-    return nulls[nulls > 0]
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
 
 
-def to_numeric(df, columns):
-    df[columns] = df[columns].apply(pd.to_numeric, errors='coerce')
-    return df
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
 
 
-def to_binary_from_nan(data, column):
-    return np.where(data[column].isna(), 0, 1)
+def plot_clustering(X_red, labels, title=None):
+    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
+    X_red = (X_red - x_min) / (x_max - x_min)
+
+    plt.figure(figsize=(6, 4))
+    for digit in digits.target_names:
+        plt.scatter(
+            *X_red[y == digit].T,
+            marker=f"${digit}$",
+            s=50,
+            c=plt.cm.nipy_spectral(labels[y == digit] / 10),
+            alpha=0.5,
+        )
+
+    plt.xticks([])
+    plt.yticks([])
+    if title is not None:
+        plt.title(title, size=17)
+    plt.axis("off")
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
