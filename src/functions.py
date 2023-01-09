d3b54d7bd65df2f16c848b70c3af7b61333466db
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2023-01-09 01:42:09 +0100

    feat: Improve analysis and refactor code

--- src/functions.py
+++ src/functions.py
@@ -1,50 +1,65 @@
-import pandas as pd
 import numpy as np
 
-from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import dendrogram
+
+def get_population_amount(df, prob_column, target_column, threshold=None):
+    return (
+        df[df[target_column].eq(1)].shape[0]
+        if threshold is None 
+        else df[df[prob_column].ge(threshold)]
+    )
 
 
-def plot_dendrogram(model, **kwargs):
-    # Create linkage matrix and then plot the dendrogram
+def get_populations(df, prob_column, target_column, acceptance_percentage,
+                    adherence_percentage=None, threshold=None):
+    # get amount of people
+    population_amount = get_population_amount(
+        df, prob_column, target_column, threshold)
 
-    # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-
-    linkage_matrix = np.column_stack(
-        [model.children_, model.distances_, counts]
-    ).astype(float)
-
-    # Plot the corresponding dendrogram
-    dendrogram(linkage_matrix, **kwargs)
+    # population who is going to be offered the plan is the most vulnerable one
+    offer_population = df.sort_values(prob_column, ascending=False)\
+                         .iloc[:population_amount]
+    # randomly select population who will accept the plan based on
+    # 85% assumption
+    acceptance_population = offer_population.sample(frac=acceptance_percentage)
+    adherence_population = None
+    if adherence_percentage:
+        adherence_population = acceptance_population.sample(
+            frac=adherence_percentage)
+    return (offer_population, acceptance_population, adherence_population)
 
 
-def plot_clustering(X_red, labels, title=None):
-    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
-    X_red = (X_red - x_min) / (x_max - x_min)
+def cost_analysis(df, prob_column, target_column, reduction_goal,
+                  acceptance_percentage, pre_cost, post_cost, threshold=None):
+    # get amount of people
+    population_amount = get_population_amount(
+        df, prob_column, target_column, threshold)
+    print(f'{population_amount=}')
+    # get baseline cost to reduce with the plan
+    baseline_cost = population_amount * post_cost
+    print(f'{baseline_cost=}')
+    # get reduced cost
+    reduced_cost = baseline_cost * reduction_goal
+    print(f'{reduced_cost=}')
+    # get amount of people that will accept the plan
+    accept_amount = population_amount * acceptance_percentage
+    # amount of people that should adhere the plan in order to achieve the goal
+    adherence_amount = (baseline_cost - reduced_cost) / (post_cost - pre_cost)
+    print(f'{adherence_amount=}')
+    # get percentage of adherance
+    adherence_percentage = adherence_amount / accept_amount
+    print(f'{adherence_percentage=}')
+    return adherence_percentage, reduced_cost
 
-    plt.figure(figsize=(6, 4))
-    for digit in digits.target_names:
-        plt.scatter(
-            *X_red[y == digit].T,
-            marker=f"${digit}$",
-            s=50,
-            c=plt.cm.nipy_spectral(labels[y == digit] / 10),
-            alpha=0.5,
-        )
 
-    plt.xticks([])
-    plt.yticks([])
-    if title is not None:
-        plt.title(title, size=17)
-    plt.axis("off")
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+def risk_analysis(offer_df, adherance_df, prob_column, risk_reduction):
+    # mean vulnerable population's risk
+    mean_baseline_risk = offer_df[prob_column].mean()
+    print(f'{mean_baseline_risk=}')
+    # reduce vulnerable population's risk
+    mean_reduced_risk = np.where(
+        offer_df.index.isin(adherance_df.index),
+        offer_df[prob_column] * risk_reduction,
+        offer_df[prob_column]
+    ).mean()
+    print(f'{mean_reduced_risk=}')
+    return mean_reduced_risk
--- src/plots.py
+++ src/plots.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import seaborn as sns
+
+from scipy.cluster.hierarchy import dendrogram
 
 def plot_features_scores(X, scores, method='univariate',
     metric_label=r'$-log(p_{value})$', figsize=(10, 20)):
@@ -13,3 +17,84 @@ def plot_features_scores(X, scores, method='univariate',
     plt.xlabel(f'{method.capitalize()} score ({metric_label})')
     plt.ylabel('')
     plt.show()
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+def plot_clustering(X_red, labels, title=None):
+    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
+    X_red = (X_red - x_min) / (x_max - x_min)
+
+    plt.figure(figsize=(6, 4))
+    for digit in digits.target_names:
+        plt.scatter(
+            *X_red[y == digit].T,
+            marker=f"${digit}$",
+            s=50,
+            c=plt.cm.nipy_spectral(labels[y == digit] / 10),
+            alpha=0.5,
+        )
+
+    plt.xticks([])
+    plt.yticks([])
+    if title is not None:
+        plt.title(title, size=17)
+    plt.axis("off")
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+
+
+def plot_risk_cost_reduction(df, prob_column, risk_reduction, base_adherance_percentage, post_cost, pre_cost):
+    fig, ax = plt.subplots(2, 1, figsize=(6, 6), sharex=True)
+
+    adherence_percentages = np.arange(base_adherance_percentage, 1.0, 0.1)
+    print(f'{adherence_percentages=}')
+
+    mean_risk_reductions = [
+        np.where(
+            df.index.isin(df.sample(frac=adherance_percentage).index),
+            df[prob_column] * risk_reduction,
+            df[prob_column]
+        ).mean()
+        for adherance_percentage in adherence_percentages
+    ]
+    print(f'{mean_risk_reductions=}')
+
+    total_population = df.shape[0]
+    cost_reductions = [
+        total_population * post_cost - total_population * adherance_percentage * (post_cost - pre_cost)
+        for adherance_percentage in adherence_percentages
+    ]
+    print(f'{cost_reductions=}')
+
+    sns.lineplot(x=adherence_percentages * 100, y=mean_risk_reductions, ax=ax[0], markers=True)
+    ax[0].xaxis.set_major_formatter(mtick.PercentFormatter())
+    ax[0].set_ylabel('Mean Risk Reduction (probability)')
+
+    sns.lineplot(x=adherence_percentages * 100, y=cost_reductions, ax=ax[1], markers=True)
+    ax[1].xaxis.set_major_formatter(mtick.PercentFormatter())
+    ax[1].set_ylabel('Cost Reduction (€)')
+
+    fig.text(0.5, 0.04, 'Percentage of adherance', ha='center')
+    fig.suptitle('Cost and risk analysis')
+
